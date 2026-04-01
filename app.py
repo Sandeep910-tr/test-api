2003477d5cad729f8a6c449be8bf69735c2f5b58
Sandeep910-tr <rathodsandeep0143@gmail.com>
2026-04-01 09:56:06 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route("/posts")
+def posts():
+    return jsonify(requests.get("https://jsonplaceholder.typicode.com/posts").json())
+
+@app.route("/comments")
+def comments():
+    return jsonify(requests.get("https://jsonplaceholder.typicode.com/comments").json())
+
+@app.route("/albums")
+def albums():
+    return jsonify(requests.get("https://jsonplaceholder.typicode.com/albums").json())
+
+if __name__ == "__main__":
+    app.run(debug=True)
