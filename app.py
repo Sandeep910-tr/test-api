683b9f52147e3a7e4583f5fc25ed1a20c082c290
Sandeep910-tr <sandeeptr@kssem.edu.in>
2026-04-01 11:32:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,15 +6,23 @@ app = Flask(__name__)
 @app.route("/posts")
 def posts():
     return jsonify(requests.get("https://jsonplaceholder.typicode.com/posts").json())
+        "data": response.json(),
+        "status": "success",
+        "status_code": 200
 
 @app.route("/comments")
 def comments():
     return jsonify(requests.get("https://jsonplaceholder.typicode.com/comments").json())
-     "status": "successfully triggered auto deployment"
+        "data": response.json(),
+        "status": "success",
+        "status_code": 200
 
 @app.route("/albums")
 def albums():
     return jsonify(requests.get("https://jsonplaceholder.typicode.com/albums").json())
+        "data": response.json(),
+        "status": "success",
+        "status_code": 200
 
 if __name__ == "__main__":
     app.run(debug=True)
